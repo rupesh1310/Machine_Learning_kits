a80a23d4759fe9054b6c21a34332786098d1301d
A.Rupesh <a.rupesh66@gmail.com>
2018-11-21 17:53:52 +0530

    updated

--- plinko/score.js
+++ plinko/score.js
@@ -1,39 +1,28 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
 
-
-function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
-  outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  console.log(outputs);
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) 
+{
+   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-	const testSetSize = 10;
+	const testSetSize = 50;
  	const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
- 
-let numberCorrect = 0;
- for (let i = 0; i < testSet.length; i++){
- 	const bucket = knn(trainingSet, testSet[i][0]);
- 	if (bucket  === testSet[i][3]) {
- 		numberCorrect++;
- 		}
- 	}
 
- 	console.log('Accuracy:', numberCorrect / testSetSize);
+ 	_.range(1, 15).forEach(k => {
  	const accuracy = _.chain(testSet)
- 	.filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+ 	.filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
  	.size()
  	.divide(testSetSize)
  	.value();
 
- 	console.log('Accuracy is', accuracy);
+ 	console.log('For k of', k, 'accuracy is', accuracy);
+  });
 }
 
-function knn(data, point) {
+
+function knn(data, point, k) {
 	return _.chain(data)
 	.map(row => [distance(row[0]), row[3]])
 	.sortBy(row => row[0])
@@ -47,16 +36,17 @@ function knn(data, point) {
 	.value();
 }
 
-
 function distance(pointA, pointB) {
 	return Math.abs(pointA - pointB);
 }
 
 function splitDataset(data, testCount) {
 	const shuffled = _.shuffle(data);
+	
 	const testSet = _.slice(shuffled, 0, testCount);
 	const trainingSet = _.slice(shuffled, testCount);
+
 	return[testSet, trainingSet];
-	
+
 }
 
