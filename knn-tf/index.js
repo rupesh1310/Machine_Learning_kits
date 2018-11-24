c7351f8fabb612c07a14aed5813cf9dc40bed747
rupesh1310 <a.rupesh66@gmail.com>
2018-11-24 13:09:17 +0530

    refactoring

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -30,7 +30,8 @@ let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv'
 features = tf.tensor(features);
 labels = tf.tensor(labels);
 
-const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
-const err = (testLabels[0][0] -result) / testLabels[0][0];
-// console.log('Guess', result, testLabels[0][0]);
+testFeatures.forEach((testPoint, i) => {
+const result = knn(features, labels, tf.tensor(testPoint), 10);
+const err = (testLabels[i][0] -result) / testLabels[i][0];
 console.log('Error', err * 100);
+});
