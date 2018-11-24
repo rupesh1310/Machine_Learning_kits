29068c51c5e4dedcd45ac732273868129a9b865a
rupesh1310 <a.rupesh66@gmail.com>
2018-11-24 14:43:45 +0530

    applying standardization

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,22 +1,28 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
-	return features
-		.sub(predictionPoint)
-		.pow(2)
-		.sum(1)
-		.pow(0.5)
-		.expandDims(1)
-		.concat(labels, 1)
-		.unstack()
-		.sort((a,b ) => a.get(0) > b.get(0) ? 1 : -1)
-		.slice(0, k)
-		.reduce((acc, pair) => acc + pair.get(1), 0) / k; 
-}
+  const { mean, variance } = tf.moments(features, 0);
 
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
+  return (
+    features
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
 
 
 let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
