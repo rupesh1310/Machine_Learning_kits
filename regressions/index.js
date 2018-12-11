401acd7c26882d635cd4ebe081911ef7fe39af33
A.Rupesh <a.rupesh66@gmail.com>
2018-12-11 18:34:28 +0530

    Update index.js

--- regressions/index.js
+++ regressions/index.js
@@ -1,18 +1,13 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
-let {
-    features,
-    labels,
-    testFeatures,
-    testLabels
-} = loadCSV(
-    '../data/cars.csv', {
-        shuffle: true,
-        splitTest: 50,
-        dataColumns: ['horsepower'],
-        labelColumns: ['mpg']
-        
 
-    }
-);
+
+let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower'],
+    labelColumns: ['mpg']
+});
+
+console.log(features, labels);
