463640a8d77983011f785176ca3ebb7253a66aab
rupesh1310 <a.rupesh66@gmail.com>
2018-12-11 18:50:34 +0530

    formulating training loop

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -3,6 +3,12 @@ class LinearRegression{
     constructor(features,labels, options) {
         this.features = features;
         this.labels = labels;
+        this.options = Object.assign({ LinearRate: 0.1, iterations: 1000}, options);
+    }
+    train() {
+        for (let i = 0; i < this.options.iterations; i++) {
+            this.gradientDescent();
+        }
     }
 }
 
