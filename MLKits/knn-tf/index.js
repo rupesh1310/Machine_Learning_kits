a142392770b9454c5f4e9dd6cfc3b1360b22a808
rupesh1310 <a.rupesh66@gmail.com>
2019-01-02 23:23:50 +0530

    mlkit

--- MLKits/knn-tf/index.js
+++ MLKits/knn-tf/index.js
@@ -24,20 +24,21 @@ function knn(features, labels, predictionPoint, k) {
   );
 }
 
-
-let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
-	shuffle: true,
-	splitTest: 10,
-	dataColumns: ['lat','long', 'sqft_lot','sqft_living'],
-	labelColumns: ['price']
-});
-
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  'kc_house_data.csv',
+  {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
+    labelColumns: ['price']
+  }
+);
 
 features = tf.tensor(features);
 labels = tf.tensor(labels);
 
 testFeatures.forEach((testPoint, i) => {
-const result = knn(features, labels, tf.tensor(testPoint), 10);
-const err = (testLabels[i][0] -result) / testLabels[i][0];
-console.log('Error', err * 100);
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const err = (testLabels[i][0] - result) / testLabels[i][0];
+  console.log('Error', err * 100);
 });
